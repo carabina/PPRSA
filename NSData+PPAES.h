58b9cc380eb4586a2e32fc58d5c23987996e2fb8
Yagiz Gurgul <yagiz@kukaapps.com>
2017-10-12 21:30:57 +0300

    Wdocumentation warns are fixed

--- NSData+PPAES.h
+++ NSData+PPAES.h
@@ -11,20 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSData (PPAES)
 
-/**
- *  AES-256 encrypt the receiver with the given key.
- *
- *  @param key: String of 32 bytes length (will be null-padded otherwise)
- *  @return A new NSData instance representing the encrypted receiver
- */
 - (NSData * __nullable)AES256EncryptWithKey:(NSString *)key;
-
-/**
- *  Decrypt an AES-256 encrypted receiver.
- *
- *  @param key: String of 32 bytes length (will be null-padded otherwise)
- *  @return A new NSData instance representing the decrypted receiver
- */
 - (NSData * __nullable)AES256DecryptWithKey:(NSString *)key;
 
 @end
